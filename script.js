e345cbdcacfbaa525a4479de1180a5a89c241ca2
André Caires <acairesm4777@gmail.com>
2025-06-18 12:20:07 -0300

    Modificações

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navbarLinks = document.getElementById('navbar-links');
+
+menuToggle.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+});
